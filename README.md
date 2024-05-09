113d674a6b2d561ec4195f59f4a452f6a401290e
DoguCigsar <dogu.cigsar@cos-soft.net>
2024-05-09 22:42:37 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# En Saglikli Yol
---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+  import { createClient } from '@supabase/supabase-js'
+
+  export const supabase = createClient('https://purxbenidnpawcrjbxbj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1cnhiZW5pZG5wYXdjcmpieGJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUyNzk5NjQsImV4cCI6MjAzMDg1NTk2NH0.3RSCjinejQuD3nsD4mxyumISEOdubL8ZfVxKpKKJ_GA')
